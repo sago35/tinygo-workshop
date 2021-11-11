00ee52475c538857d48e0d6e918265585032946d
sago35 <sago35@gmail.com>
2021-11-11 20:34:47 +0900

    printf debug

--- 01_blinky/main.go
+++ 01_blinky/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"machine"
 	"time"
 )
@@ -9,7 +10,10 @@ func main() {
 	led := machine.LED
 	//led := machine.LCD_BACKLIGHT
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	cnt := 0
 	for {
+		cnt++
+		fmt.Printf("cnt %d\r\n", cnt)
 		led.Low()
 		time.Sleep(time.Millisecond * 500)
 
