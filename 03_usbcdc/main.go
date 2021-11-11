af73fd7fda76517fdaa579b66854bcc382b35f24
sago35 <sago35@gmail.com>
2021-11-11 20:47:43 +0900

    03_usbcdc

--- 03_usbcdc/main.go
+++ 03_usbcdc/main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"machine"
+	"time"
 )
 
 func main() {
-	usbcdc := machine.Serial
+	led := machine.LCD_BACKLIGHT
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	led.High()
 
+	usbcdc := machine.Serial
+	usbcdc.Configure(machine.UARTConfig{})
+
+	input := make([]byte, 64)
+	i := 0
 	for {
 		if usbcdc.Buffered() > 0 {
 			data, _ := usbcdc.ReadByte()
 
-			// just echo the character
-			usbcdc.WriteByte(data)
+			switch data {
+			case 13:
+				// return key
+				usbcdc.Write([]byte("\r\n"))
+
+				switch string(input[:i]) {
+				case "on":
+					led.High()
+				case "off":
+					led.Low()
+				case "toggle", "t":
+					led.Toggle()
+				}
+				i = 0
+			default:
+				// just echo the character
+				usbcdc.WriteByte(data)
+				input[i] = data
+				i++
+			}
 		}
+		time.Sleep(10 * time.Millisecond)
 	}
 }
