4a23c84493b579044b323445bcd09194a39e5835
sago35 <sago35@gmail.com>
2021-11-11 20:49:23 +0900

    04_systick

---
+++ 04_systick/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"device/arm"
+	"machine"
+)
+
+var timerCh = make(chan struct{}, 1)
+
+func main() {
+	machine.LED.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	// timer fires 10 times per second
+	arm.SetupSystemTimer(machine.CPUFrequency() / 10)
+
+	for {
+		machine.LED.Low()
+		<-timerCh
+		machine.LED.High()
+		<-timerCh
+	}
+}
+
+//export SysTick_Handler
+func timer_isr() {
+	select {
+	case timerCh <- struct{}{}:
+	default:
+		// The consumer is running behind.
+	}
+}
