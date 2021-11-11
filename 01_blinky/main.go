8a12bcd9b908be19a74564a8ead9c66a0f2304f3
sago35 <sago35@gmail.com>
2021-11-11 20:42:17 +0900

    buzzer

--- 01_blinky/main.go
+++ 01_blinky/main.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"machine"
 	"time"
+
+	"tinygo.org/x/drivers/buzzer"
 )
+
+type note struct {
+	tone     float64
+	duration float64
+}
 
 func main() {
 	led := machine.LED
 	//led := machine.LCD_BACKLIGHT
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	cnt := 0
+
+	bzrPin := machine.WIO_BUZZER
+	bzrPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	bzr := buzzer.New(bzrPin)
+
+	notes := []note{
+		{buzzer.C3, buzzer.Quarter},
+		{buzzer.D3, buzzer.Quarter},
+		{buzzer.E3, buzzer.Quarter},
+	}
+
 	for {
 		cnt++
 		fmt.Printf("cnt %d\r\n", cnt)
@@ -19,5 +37,10 @@ func main() {
 
 		led.High()
 		time.Sleep(time.Millisecond * 500)
+
+		for _, n := range notes {
+			bzr.Tone(n.tone, n.duration)
+			time.Sleep(10 * time.Millisecond)
+		}
 	}
 }
